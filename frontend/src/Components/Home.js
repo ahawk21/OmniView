33c20be92e86f456f4d51ada4661122c09f7ff19
Ahawkfizz <001.aqh@gmail.com>
2019-08-15 15:19:57 -0500

    sidebar

--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -2,8 +2,18 @@ import React from "react";
 import './Home.css';
 
 
+
 export default class Home extends React.Component {
 
+    openNav(){ 
+         // Sidebar length
+        document.getElementById("mySidenav").style.width = "250px";
+    }
+
+    closeNav(){
+         // Sidebar 
+        document.getElementById("mySidenav").style.width = "0px";
+    }
 
     render(){
         return(
@@ -14,10 +24,19 @@ export default class Home extends React.Component {
                 <style>{'body { background-color: white; width: 100%; height: 100%; } html {background-color: red; width: 100%; height: 100%;}'}</style>
                     
                 </header>
-                <body>
-                    <h1>"INSERT GLOBE HERE"</h1>
-                </body>
+                <body >
+                    <div id="mySidenav" className="sidenav">
+                        <a href="javascript:void(0)" class="closebtn" onClick={this.closeNav}>&times;</a>
+                        <a href="#">News</a>
+                            <br></br>
+                        <a href="#">Photos</a>
+                            <br></br>
+                        <a href="#">Video</a>
+                            <br></br>
+                    </div>
 
+                    <span style={{fontSize: '30px', cursor: 'pointer', width: 'absolute'}} onClick={this.openNav}>&#9776;</span>                
+                </body>
             </div>
         )
     }
